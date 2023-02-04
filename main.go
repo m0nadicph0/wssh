5b9c9eac3136eed2f584ea66951e7813171e35a5
m0nadicph0 <m0nadic.ph0@gmail.com>
2023-02-04 15:30:20 +0530

    add ws server

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"os"
 	"strings"
 )
 
 func main() {
 
+	wss := NewWSServer("0.0.0.0", 9696, func(clientID string, messageType int, message []byte) {
+		red := color.New(color.FgRed).SprintFunc()
+		fmt.Printf("[%s] %s", red(clientID), string(message))
+	})
+
+	wss.Start()
+
 	shell := NewShell(func(tokens []string) {
-		println(strings.Join(tokens, " "))
+		//println(strings.Join(tokens, " "))
+		clientID := tokens[0]
+		wss.WriteText(clientID, strings.Join(tokens[1:], " "))
 	})
 
 	err := shell.Start()
